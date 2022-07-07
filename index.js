3bf4400c7c4565818b110bf02b3138f1f3d581ed
AzizRAA <azizbekruzmatovraa@gmail.com>
2022-07-07 12:18:20 +0300

    added nav

--- index.js
+++ index.js
@@ -71,3 +71,26 @@ document.querySelectorAll('#delete').forEach((button, id) => {
     Books.retrieve();
   });
 });
+
+const app = {
+    pages: [],
+    show: new Event('show'),
+    init: function(){
+        app.pages = document.querySelectorAll('.page');
+        app.pages.forEach((pg)=>{
+            pg.addEventListener('show', app.pageShown);
+        })
+        
+        document.querySelectorAll('.nav-link').forEach((link)=>{
+            link.addEventListener('click', app.nav);
+        })
+    },
+    nav: function(ev){
+        ev.preventDefault();
+        let currentPage = ev.target.getAttribute('data-target');
+        document.querySelector('.active').classList.remove('active');
+        document.getElementById(currentPage).classList.add('active');
+    },
+}
+
+document.addEventListener('DOMContentLoaded', app.init);
